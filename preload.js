cd7a92ad5edd839dae99b81c12a416d0cf60816f
besiktaş1903 <acaroktay1967@gmail.com>
2026-01-11 22:37:36 +0300

    Create preload.js

---
+++ preload.js
@@ -0,0 +1,41 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('api', {
+  // Bilişim Suçları
+  bilişim: {
+    getAll: () => ipcRenderer.invoke('bilişim:getAll'),
+    save: (data) => ipcRenderer.invoke('bilişim:save', data),
+    delete: (id) => ipcRenderer.invoke('bilişim:delete', id)
+  },
+  
+  // Nitelikli Dolandırıcılık
+  dolandırıcılık: {
+    getAll: () => ipcRenderer.invoke('dolandırıcılık:getAll'),
+    save: (data) => ipcRenderer.invoke('dolandırıcılık:save', data)
+  },
+  
+  // Kredi Kartı Suçları
+  krediKartı: {
+    getAll: () => ipcRenderer.invoke('kredi-kartı:getAll'),
+    save: (data) => ipcRenderer.invoke('kredi-kartı:save', data)
+  },
+  
+  // Yedekleme
+  backup: {
+    create: () => ipcRenderer.invoke('backup:create'),
+    restore: (file) => ipcRenderer.invoke('backup:restore', file)
+  },
+  
+  // Raporlar
+  report: {
+    generate: (options) => ipcRenderer.invoke('report:generate', options)
+  },
+  
+  // Menü olayları
+  onMenuEvent: (channel, callback) => {
+    const validChannels = ['menu:backup', 'menu:restore', 'menu:report-all', 'menu:report-bilisim'];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.on(channel, (event, ...args) => callback(...args));
+    }
+  }
+});
